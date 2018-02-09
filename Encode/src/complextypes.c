8fbe1f3a40844bfaf650d7b5e1c23956aa17fc44
miguelsilva96 <a74601@alunos.uminho.pt>
2018-02-09 19:16:23 +0000

    Add functions to create complextypes
    * Still need to do the Opaque_t type, all others are done:
    -- Nothing
    -- IpAddress
    -- CounterValue
    -- BigCounterValue
    -- Unsigned32

--- Encode/src/complextypes.c
+++ Encode/src/complextypes.c
@@ -1,24 +1,74 @@
 
 #include <complextypes.h>
 
-ApplicationSyntax_t* createNothing() {
-    ApplicationSyntax *res;
+/**
+* Private function to allocate space for an ApplicationSyntax_t
+*/
+ApplicationSyntax_t* allocate() {
+    ApplicationSyntax_t *res;
+    res = (ApplicationSyntax_t*) malloc(sizeof(ApplicationSyntax_t));
+    return res;
+}
 
-//    res -> present = ApplicationSyntax_PR_ipAddress_value;
-//    res -> choice.ipAddress_value = (IpAddress_t) octetString;
+ApplicationSyntax_t* createNothing() {
+    ApplicationSyntax_t *res = allocate();
+    res -> present = ApplicationSyntax_PR_NOTHING;
     return res;
 }
 
 ApplicationSyntax_t* createIpAddress(char* ipAddress) {
-    ApplicationSyntax *res;
+    ApplicationSyntax_t *res = allocate();
     int size = strlen(ipAddress);
     OCTET_STRING_t* octetString = malloc(sizeof(OCTET_STRING_t) * size);
-    // -1 de error
-    // 0 se correu bem
-    // nao esquecer de verificar
-    OCTET_STRING_fromString(octetString, ipAddress);
-    res = malloc(sizeof(ApplicationSyntax));
+    int code = OCTET_STRING_fromString(octetString, ipAddress);
+    if(code < 0) {
+        res -> present = ApplicationSyntax_PR_NOTHING;
+        return res;
+    }
     res -> present = ApplicationSyntax_PR_ipAddress_value;
-    res -> choice.ipAddress_value = (IpAddress_t) octetString;
+    res -> choice.ipAddress_value = (IpAddress_t) *octetString;
     return res;
 }
+
+ApplicationSyntax_t* createCounterValue(long counterValue) {
+    ApplicationSyntax_t *res = allocate();
+    Counter32_t counter;
+    // Counter32_t is a simple long, so it can be casted
+    counter = (Counter32_t) counterValue;
+    res -> present = ApplicationSyntax_PR_counter_value;
+    res -> choice.counter_value = counter;
+    return res;
+}
+
+ApplicationSyntax_t* createBigCounterValue(uint8_t *counterValue, size_t size) {
+    ApplicationSyntax_t *res = allocate();
+    ASN__PRIMITIVE_TYPE_t *baseType;
+    INTEGER_t counter;
+    baseType = (ASN__PRIMITIVE_TYPE_t*) malloc(sizeof(ASN__PRIMITIVE_TYPE_t));
+    baseType -> buf = counterValue;
+    baseType -> size = size;
+    counter = (INTEGER_t) *baseType;
+    res -> present = ApplicationSyntax_PR_big_counter_value;
+    res -> choice.big_counter_value = (Counter64_t) counter;
+    return res;
+}
+
+ApplicationSyntax_t* createTimeTicks(long timeTicks) {
+    ApplicationSyntax_t *res = allocate();
+    // TimeTicks_t is a simple long, so it can be casted
+    TimeTicks_t timeTicksValue = (TimeTicks_t) timeTicks;
+    res -> present = ApplicationSyntax_PR_timeticks_value;
+    res -> choice.timeticks_value = timeTicksValue;
+    return res;
+}
+
+ApplicationSyntax_t* createUnsigned32(unsigned long value) {
+    ApplicationSyntax_t *res = allocate();
+    // Unsigned32_t is an unsigned long, so it can be casted
+    Unsigned32_t valueRes = (Unsigned32_t) value;
+    res -> present = ApplicationSyntax_PR_unsigned_integer_value;
+    res -> choice.unsigned_integer_value = valueRes;
+    return res;
+}
+
+// TO DO OPAQUE
--- Encode/src/complextypes.h
+++ Encode/src/complextypes.h
@@ -3,13 +3,51 @@
 #define	_COMPLEXTYPES_H_
 
 #include <OCTET_STRING.h>
-#include <IpAddress.h>
 #include <ApplicationSyntax.h>
 
+/**
+* Function that creates an ApplicationSyntax_t setting its present
+* value to NOTHING and returns a pointer to it.
+* @return Returns an ApplicationSyntax_t with nothing in it
+*/
 ApplicationSyntax_t* createNothing();
-/*
-    ipAddress example "192.136.23.23" or "localhost"
+
+/**
+* Funtion that creates an ApplicationSyntax_t with an IpAddress_t
+* and returns a pointer to it.
+* @params ipAddress - string that specifies the ipAddress
+*                    ("192.136.23.23" or "localhost" for example)
+* @returns ApplicationSyntax_t pointer to NOTHING if the string is not valid
+*          or to an ApplicationSyntax_t with the given IpAddress value
 */
 ApplicationSyntax_t* createIpAddress(char* ipAddress);
+
+/**
+* Function that creates an ApplicationSyntax_t with a normal counter
+* (Counter32_t - same as a long), returning a pointer to it.
+* @param counterValue - value of the counter
+* @returns ApplicationSyntax_t pointer to the value created
+*/
+ApplicationSyntax_t* createCounterValue(long counterValue);
+
+/**
+* Function that creates an ApplicationSyntax_t with a big counter
+* (Counter64_t), returning a pointer to it.
+* @param counterValue - value of the counter
+* @param size - size of the counter in bytes
+* @returns ApplicationSyntax_t pointer to the value created
+*/
+ApplicationSyntax_t* createBigCounterValue(uint8_t *counterValue, size_t size);
+
+
+/**
+* Function that creates an ApplicationSyntax_t with an unsigned int 32bit
+* (Unsigned32_t aka unsigned long), returning a pointer to it
+* @param value - unsigned value
+* @returns ApplicationSyntax_t pointer to the value created
+*/
+ApplicationSyntax_t* createUnsigned32(unsigned long value);
+
+
 
 #endif
